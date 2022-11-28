18832000acca82623ddff198622e0d9242ab545a
clew <lewis@clewup.co.uk>
2022-11-28 16:44:59 +0000

    Made Base64 non nullable for ImageModel.

--- ecommerce.api/Classes/ImageModel.cs
+++ ecommerce.api/Classes/ImageModel.cs
@@ -2,6 +2,6 @@ namespace ecommerce.api.Classes;
 
 public class ImageModel
 {
-    public string? Base64 { get; set; }
+    public string Base64 { get; set; } = "";
     public string? Url { get; set; }
 }
