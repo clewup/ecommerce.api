58e6db51bb1f80b1503c7d8d41f0ad7d07cf389e
clew <lewis@clewup.co.uk>
2022-12-04 17:37:04 +0000

    Removed CORs policy.

--- ecommerce.api/Program.cs
+++ ecommerce.api/Program.cs
@@ -6,26 +6,10 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var  CorsPolicy = "_corsPolicy";
-
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-// Cors
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy(name: CorsPolicy,
-        policy  =>
-        {
-            policy.WithOrigins(
-                    "http://localhost:3000",
-                    "https://localhost:3000")
-                .AllowAnyHeader()
-                .AllowAnyMethod();
-        });
-});
 
 // Cloudinary
 var cloudName = builder.Configuration.GetValue<string>("CloudinaryConfig:CloudName");
@@ -56,8 +40,6 @@ var app = builder.Build();
 
 app.UseSwagger();
 app.UseSwaggerUI();
-
-app.UseCors(CorsPolicy);
 
 app.UseHttpsRedirection();
 
