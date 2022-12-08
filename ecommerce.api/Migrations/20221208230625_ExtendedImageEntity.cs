a61b94d4fd9941601d2f67626fadaed79e7dfe52
clew <lewis@clewup.co.uk>
2022-12-08 23:06:45 +0000

    Extended image entity.

---
+++ ecommerce.api/Migrations/20221208230625_ExtendedImageEntity.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ecommerce.api.Migrations
+{
+    /// <inheritdoc />
+    public partial class ExtendedImageEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Images",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "Images",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Images");
+
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "Images");
+        }
+    }
+}
